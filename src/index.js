f788551ff1ab5d7baf91cc4fe9b76c97f5231527
Maxim Belyakov <maxim.a.belyakov+g@gmail.com>
2020-03-18 20:30:03 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import Playground from './Playground'
+
+ReactDOM.render(<Playground />, document.getElementById("root"))
