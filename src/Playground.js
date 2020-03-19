db2a74c247f5f6c7972d72f71e0ca14cf5500263
Maxim Belyakov <maxim.a.belyakov+g@gmail.com>
2020-03-19 22:59:08 +0200

    useEffect

--- src/Playground.js
+++ src/Playground.js
@@ -1,13 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import randomColor from 'randomcolor'
 
 export default function Playground() {
   const [count, setCount] = useState(0)
+  
+  const [color, setColor] = useState(null)
+  useEffect(() => {
+    setColor(randomColor())
+  }, [count])
+  
   return (
-    <div>
+    <div style={{ borderTop: `10px solid ${color}`}}>
       {count}
-      <button onClick={() => setCount(count - 1)}>-</button>
-      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(currentCount => currentCount - 1)}>-</button>
+      <button onClick={() => setCount(currentCount => currentCount + 1)}>+</button>
     </div>
   )
 }
