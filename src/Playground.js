72081a4338ef6e3cbbb0a6d4c8cf157fbd3cf544
Maxim Belyakov <58391051+maxim-belyakov@users.noreply.github.com>
2020-03-22 17:35:19 +0200

    useCallback, useMemo

--- src/Playground.js
+++ src/Playground.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef, useCallback, useMemo } from 'react'
 import randomColor from 'randomcolor'
 
 export default function Playground() {
@@ -6,19 +6,26 @@ export default function Playground() {
   
   const inputRef = useRef()
   
-  const [color, setColor] = useState(null)
-  useEffect(() => {
-    setColor(randomColor())
-    inputRef.current.focus()
-  }, [count])
+  const [color, setColor] = useState(randomColor())
+  useEffect(() => inputRef.current.focus(), [count])
+  
+  useCallback(() => console.log('useCallback')) // return the function
+  useMemo(() => () => console.log('useMemo')) // return the result of the function
   
   return (
     <div style={{ borderTop: `10px solid ${color}`}}>
       {count}
       <button onClick={() => setCount(currentCount => currentCount - 1)}>-</button>
       <button onClick={() => setCount(currentCount => currentCount + 1)}>+</button>
+      <button onClick={() => setColor(randomColor())}>Change Color</button>
       <hr />
       <input ref={inputRef} type="range" onChange={e => setCount(e.target.value)} value={count} />
     </div>
   )
 }
+
+const Calculate = React.memo(({ cb, num }) => {
+  cb(num)
+  const renderCount = useRef(1)
+  return <div>{renderCount.current++}</div>
+})
