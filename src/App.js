d29b0c1f14b69293dc10568905d781c17efc91e4
Maxim Belyakov <58391051+maxim-belyakov@users.noreply.github.com>
2020-06-10 14:25:08 +0300

    just for fun

--- src/App.js
+++ src/App.js
@@ -2,5 +2,27 @@ import React from 'react'
 import Paint from './components/Paint'
 
 export default function App() {
-  return <Paint />
+  return (
+    <main>
+      <h1>Maxim Belyakov</h1>
+      <p class="lead">My passion is creating, and I find happiness in being a better me today than the me yesterday.</p>
+      <p class="quote">Life is 10% what happens to you and 90% how you react to it.</p>
+      <p> 🚀 👽 👾 </p>
+      <section>
+      <h1 class="check">Portfolio projects</h1>
+      </section>
+      <section>
+        <h1 class="check">Links</h1>
+        <ul>
+          <li> <a href="https://linkedin.com/in/maxim-belyakov">LinkedIn</a> </li>
+          <li> <a href="mailto:maxim.a.belyakov@gmail.com?subject=Hey, Maxim!">E-mail</a> </li>
+          <li> <a href="https://medium.com/@maxim.a.belyakov">Medium</a> </li>
+          <li> <a href="https://instagram.com/maxim_chai">Instagram</a> </li>
+          <li> <a href="https://github.com/maxim-belyakov">GitHub</a> </li>
+          <li> <a href="https://twitter.com/maxim_chai">Twitter</a> </li>
+          <li> <a href="https://facebook.com/maxim.a.belyakov">Facebook</a> </li>
+        </ul>
+      </section>
+    </main>
+  )
 }
